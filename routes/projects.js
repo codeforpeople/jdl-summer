f37f0b84f7be42686714b87abc72eec2a3e0aece
Alexandru Onea <onea.alex@gmail.com>
2014-04-19 21:31:04 +0100

    reading from file implemented

--- routes/projects.js
+++ routes/projects.js
@@ -1,5 +1,11 @@
+var fs = require('fs');
+
 var projects = function (req, res) {
-	res.render('projects', { title: 'Junior Development Lab' });
+
+	var data = fs.readFileSync('data/projects.json');
+	data = JSON.parse(data);
+
+	res.render('projects', { title: 'Junior Development Lab', data: data });
 };
 
 module.exports = projects;
