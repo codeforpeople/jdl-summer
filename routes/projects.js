17e42db5661a94398a9f30b23a13c918118f6196
Alexandru Onea <onea.alex@gmail.com>
2014-04-21 17:45:13 +0100

    added detailed view of the projects

--- routes/projects.js
+++ routes/projects.js
@@ -4,6 +4,18 @@ var projects = function (req, res) {
 
 	var data = fs.readFileSync('data/projects.json');
 	data = JSON.parse(data);
+	data.index = -1;
+
+	if (typeof req.query.id != 'undefined') {
+		var id = req.query.id;
+
+		for (var i = 0; i < data.projects.length; i++) {
+			if (data.projects[i].id === id) {
+				data.index = i;
+				break;
+			}
+		};
+	}
 
 	res.render('projects', { title: 'Junior Development Lab', data: data });
 };
