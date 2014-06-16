75d04204fcaad685a4bbadfac4fb0ef5c11b9b28
Alexandru Onea <onea.alex@gmail.com>
2014-06-16 21:19:02 +0100

    changed url

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 
 var FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID.toString();
 var FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET.toString();
-var FACEBOOK_CALLBACK = 'http://www.jdl.ro/auth/facebook/callback';
+var FACEBOOK_CALLBACK = 'http://jdl.ro/auth/facebook/callback';
 
 routes.sync = require('./routes/sync');
 routes.check = require('./routes/check');
